324d592b90bd0e2d73078db5a222af2d773749a6
Kenny Luong <83483633+Zen-Ken@users.noreply.github.com>
2022-12-02 22:23:25 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useFormik } from "formik";
+// TODO: import useFormik from formik library
+
+function App() {
+  const [errors, setErrors] = useState({emailError: '', pswError: ''})
+   const formik = useFormik({
+    initialValues: { email: '', password: ''},
+    onSubmit: values => {
+      setErrors({...emailValidation(values.email), ...pswValidation(values.password)})
+    }
+  })
+  
+  function emailValidation(email){
+    let message = {}
+    if(email == ''){
+      message = {emailError:"Field Required"}
+    }
+    else{
+      message = {emailError:""}
+    }
+    return message;
+  }
+
+  function pswValidation(psw){
+    let message = {}
+    if(psw == ''){ 
+      message = {pswError:"Field Required"}   }
+    else{
+      message = {pswError:""}
+    }
+    return message;
+  }
+  
+
+  return (
+    <div>
+      <p>
+        The app is ready! You can proceed with the task instructions. TODO:
+        build you form here.
+      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email</div>
+        <input type="text" id="emailField" name="email" value={formik.values.email} onChange={formik.handleChange} />
+        <div style={{color: "red"}}>{errors.emailError}</div>
+        <div>Password</div>
+        <input type="text" id="pswField" name="password" value={formik.values.password} onChange={formik.handleChange} />
+        <div style={{color: "red"}}>{errors.pswError}</div>
+        <button type="submit" id="submitBtn">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
